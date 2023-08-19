806f5274811f736b8660a3e68ebad7b12a766e78
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-08-19 10:29:28 +0530

    Create bmi.java

---
+++ Day1/bmi.java
@@ -0,0 +1,16 @@
+//BMI Calculator
+
+import java.util.;
+public class Main
+{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+float a,b,d;
+System.out.println("Your height in m:");
+a=sc.nextFloat();
+System.out.println("Your weight in kg : ");
+b=sc.nextFloat();
+d=b/(aa);
+System.out.println("Your BMI is :"+d);
+}
+}
