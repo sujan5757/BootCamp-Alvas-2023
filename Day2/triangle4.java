7a9b99d95d61bc4c5fd996f4891602cc3cbe8c72
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-08-19 11:18:05 +0530

    Create triangle4.java

---
+++ Day2/triangle4.java
@@ -0,0 +1,29 @@
+/*
+1
+
+1 2
+
+1 2 3
+
+1 2 3 4
+*/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m=1;
+        int n=sc.nextInt();
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(m);
+               m++; 
+            }
+            m=1;
+            System.out.println();
+        }
+	}
+}
