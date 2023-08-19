dd89217e2ce55e36f1bac0e11524e4c3da8d6b0c
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-08-19 10:28:33 +0530

    Create ageproblem.java

---
+++ Day1/ageproblem.java
@@ -0,0 +1,18 @@
+//age  problem normal
+
+import java.util.*;
+public class Main
+{
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a,c;
+String b;
+System.out.println("How old are you?");
+a=sc.nextInt();
+System.out.println("How tall are you ?");
+b=sc.next();
+System.out.println("How much do you weight?");
+c=sc.nextInt();
+System.out.println("So,youre"+a+"old,"+b+"tall and "+c+"heavy.");
+}
+}
